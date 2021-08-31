362867142387c86ed0f698f9d2396491ae11306f
cfrazier1978 <66221343+cfrazier1978@users.noreply.github.com>
2021-08-31 13:20:51 -0400

    Update labs.py

--- labs.py
+++ labs.py
@@ -1,6 +1,6 @@
 # Example labs.py
 # Import the file that defines our instance of the Experiment class (exp)
-from models.config import exp
+from models.config import experiment
 
 # The root directory of our model code
-model_dir = "mod els"
+model_dir = "models"
