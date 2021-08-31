5552edb8448fd968c7c2143aa86310b66ab96571
cfrazier1978 <66221343+cfrazier1978@users.noreply.github.com>
2021-08-31 15:30:54 -0400

    Delete labs.py

--- labs.py
+++
@@ -1,7 +0,0 @@
-# Example labs.py
-# Import the file that defines our instance of the Experiment class (exp)
-from models.config import exp
-
-# The root directory of our model code
-model_dir = "models"
-print('Hello')
