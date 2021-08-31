37acbd5119ecfd32d9efa067632103d45542d85a
cfrazier1978 <66221343+cfrazier1978@users.noreply.github.com>
2021-08-31 10:05:41 -0400

    Update labs.py

--- labs.py
+++ labs.py
@@ -1,1 +1,6 @@
-junk text
+# Example labs.py
+# Import the file that defines our instance of the Experiment class (exp)
+from models.config import exp
+
+# The root directory of our model code
+model_dir = "JUNK"
