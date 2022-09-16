7ecb8a9b6b8b6970ccac22b79372ae25b49c8ac1
BlockScience Labs <jupyter@blocksciencelabs.com>
2022-09-16 04:10:32 +0000

    Test

--- docs/js/analysis.js
+++ docs/js/analysis.js
@@ -1,9 +1,8 @@
 'use strict';
 
 let reserveSelection = document.getElementById('reserve-selection');
-
 reserveSelection.addEventListener('change', handleReserveChange);
 
 function handleReserveChange(event) {
-    console.log(event.value);
+    console.log(event);
 }
