7c261e22fd9bc4aa23fe7612a048a7b956fc6459
BlockScience Labs <jupyter@blocksciencelabs.com>
2022-09-16 04:16:50 +0000

    Test

--- docs/js/analysis.js
+++ docs/js/analysis.js
@@ -1,8 +1,14 @@
 'use strict';
 
-let reserveSelection = document.getElementById('reserve-selection');
+const reserveSelection = document.getElementById('reserve-selection');
+const riverSelection = document.getElementByName('river-selection');
 reserveSelection.addEventListener('change', handleReserveChange);
+riverSelection.addEventListener('change', handleRiverChange);
 
 function handleReserveChange(event) {
+    console.log(event.target.value);
+}
+
+function handleRiverChange(event) {
     console.log(event.target.value);
 }
