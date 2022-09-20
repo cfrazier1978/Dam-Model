1be56cf0635d1eae8120bcac4be56d60517f865c
BlockScience Labs <jupyter@blocksciencelabs.com>
2022-09-20 18:50:00 +0000

    Test

--- docs/data/fetch.py
+++ docs/data/fetch.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 labs = Client()
 
-df = pd.DataFrame(labs.fetch_results(182))
+df = pd.DataFrame(labs.fetch_results(183))
 
 df.to_csv('simulation.csv')
--- docs/js/analysis.js
+++ docs/js/analysis.js
@@ -105,19 +105,11 @@ window.addEventListener('load', () => {
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
         //Read the data
-        d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+        d3.csv(`data/simulation.csv`, function(data) {
 
-          // When reading the csv, I must format variables:
-          function(d){
-            return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-          }).then(
-
-          // Now I can use this dataset:
-          function(data) {
-
-            // Add X axis --> it is a date format
-            const x = d3.scaleTime()
-              .domain(d3.extent(data, function(d) { return d.date; }))
+            // Add X axis -> timestep
+            const x = d3.ScaleLinear()
+              .domain(d3.extent(data, function(d) { return d.timestep; }))
               .range([ 0, width ]);
             svg.append("g")
               .attr("transform", `translate(0, ${height})`)
@@ -125,7 +117,7 @@ window.addEventListener('load', () => {
 
             // Add Y axis
             const y = d3.scaleLinear()
-              .domain([0, d3.max(data, function(d) { return +d.value; })])
+              .domain([0, d3.max(data, function(d) { return +d.Reservoir_Level; })])
               .range([ height, 0 ]);
             svg.append("g")
               .call(d3.axisLeft(y));
@@ -137,8 +129,8 @@ window.addEventListener('load', () => {
               .attr("stroke", "steelblue")
               .attr("stroke-width", 1.5)
               .attr("d", d3.line()
-                .x(function(d) { return x(d.date) })
-                .y(function(d) { return y(d.value) })
+                .x(function(d) { return x(d.timestep) })
+                .y(function(d) { return y(d.Reservoir_Level) })
                 );
         });
     }
